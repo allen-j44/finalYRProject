ad758d3c7cf2bb9a1636afafc38895a0d910562f
Jack Allen <152200136+allen-j44@users.noreply.github.com>
2024-03-29 15:07:06 +0000

    defs added

--- BackEnd/defenders.py
+++ BackEnd/defenders.py
@@ -12,7 +12,118 @@ collection = db["defsCollection"]
 
 # Your dataset with custom _id field
 
-data = []
+
+data = [
+    {
+        "_id" : ObjectId(),
+        "Team": "1s",
+        "Name": "Jordan Mills",
+        "Age": 33,
+        "POS": "DEF",
+        "App": 14,
+        "Clean Sheets": 2,
+        "Yellows": 0,
+        "Reds": 0,
+        "Mins": 1214
+    },
+    {
+        "_id" : ObjectId(),
+        "Team": "1s",
+        "Name": "Kyle Moore",
+        "Age": 22,
+        "POS": "DEF",
+        "App": 13,
+        "Clean Sheets": 2,
+        "Yellows": 0,
+        "Reds": 0,
+        "Mins": 682
+    },
+    {
+        "_id" : ObjectId(),
+        "Team": "1s",
+        "Name": "Nathan Crawford",
+        "Age": 29,
+        "POS": "DEF",
+        "App": 14,
+        "Clean Sheets": 2,
+        "Yellows": 1,
+        "Reds": 0,
+        "Mins": 1125
+    },
+    {
+        "_id" : ObjectId(),
+        "Team": "2s",
+        "Name": "Karl Johnston",
+        "Age": 40,
+        "POS": "DEF",
+        "App": 9,
+        "Clean Sheets": 1,
+        "Yellows": 1,
+        "Reds": 0,
+        "Mins": 723
+    },
+    {
+        "_id" : ObjectId(),
+        "Team": "1s",
+        "Name": "Michael Keable",
+        "Age": 31,
+        "POS": "DEF",
+        "App": 12,
+        "Clean Sheets": 2,
+        "Yellows": 3,
+        "Reds": 0,
+        "Mins": 1001
+    },
+    {
+        "_id" : ObjectId(),
+        "Team": "",
+        "Name": "Craig McCullough",
+        "Age": 37,
+        "POS": "DEF",
+        "App": 4,
+        "Clean Sheets": 0,
+        "Yellows": 0,
+        "Reds": 0,
+        "Mins": 360
+    },
+    {
+        "_id" : ObjectId(),
+        "Team": "2s",
+        "Name": "Danny Smyth",
+        "Age": 30,
+        "POS": "DEF",
+        "App": 1,
+        "Clean Sheets": 0,
+        "Yellows": 0,
+        "Reds": 0,
+        "Mins": 24
+    },
+    {
+        "_id" : ObjectId(),
+        "Team": "2s",
+        "Name": "Michael Massey",
+        "Age": 38,
+        "POS": "DEF",
+        "App": 6,
+        "Clean Sheets": 0,
+        "Yellows": 2,
+        "Reds": 2,
+        "Mins": 266
+    },
+    {
+        "_id" : ObjectId(),
+        "Team": "1s",
+        "Name": "Nathan Shaw",
+        "Age": 32,
+        "POS": "DEF",
+        "App": 8,
+        "Clean Sheets": 0,
+        "Yellows": 1,
+        "Reds": 0,
+        "Mins": 518
+    }
+]
+
 
 
 # Insert the data into the collection
