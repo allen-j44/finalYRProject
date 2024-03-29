dda75692624e51a7c9785ab9726a8a0a0e1ae60b
Jack Allen <152200136+allen-j44@users.noreply.github.com>
2024-03-29 14:32:01 +0000

    gks updated

---
+++ BackEnd/defenders.py
@@ -0,0 +1,22 @@
+import pymongo
+from bson import ObjectId  # Import the ObjectId class
+
+# Replace these values with your actual MongoDB connection details
+mongo_client = pymongo.MongoClient("mongodb://127.0.0.1:27017")
+
+# Access or create a database (replace 'your_database' with your actual database name)
+db = mongo_client["ProjectDB"]
+
+# Access or create a collection within the database (replace 'your_collection' with your actual collection name)
+collection = db["defsCollection"]
+
+# Your dataset with custom _id field
+
+data = []
+
+
+# Insert the data into the collection
+collection.insert_many(data)
+
+# Close the MongoDB connection
+mongo_client.close()
--- BackEnd/staff.py
+++ BackEnd/staff.py
@@ -7,3 +7,50 @@ staff = db.staffCollection
 
 #Create Dataset for the staff used for authentication later
 
+staff_list = [
+          { 
+            "name" : "Martin O'Neill",
+            "username" : "moneill",  
+            "password" : b"m_oneill",
+            "role" : "Manager",
+            "email" : "moneill@IFAemail.com",
+            "admin" : False
+          },
+          { 
+            "name" : "Jimmy Nicholl",
+            "username" : "jnicholl",  
+            "password" : b"j_nicholl",
+            "role" : "Assistant Manager",
+            "email" : "jnicholl@IFAemail.com",
+            "admin" : False
+          },
+          { 
+            "name" : "Aaron Hughes",
+            "username" : "ahughes",  
+            "password" : b"a_hughes",
+            "role" : "Senior Coach",
+            "email" : "ahughes@IFAemail.com",
+            "admin" : False
+          },        
+          { 
+            "name" : "Diarmuld O'Carroll",
+            "username" : "docarroll",  
+            "password" : b"d_ocarroll",
+            "role" : "Senior Coach",
+            "email" : "docarroll@IFAemail.com",
+            "admin" : True
+          },
+          { 
+            "name" : "David White",
+            "username" : "dwhite",  
+            "password" : b"d_white",
+            "role" : "Team Doctor",
+            "email" : "dwhite@IFAemail.com",
+            "admin" : False
+          }
+       ]
+
+#Encrpyt hash password
+for new_staff_user in staff_list:
+      new_staff_user["password"] = bcrypt.hashpw(new_staff_user["password"], bcrypt.gensalt())
+      staff.insert_one(new_staff_user)
