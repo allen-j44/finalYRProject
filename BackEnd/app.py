9e9c3fa49522c0e7f54300fa9374f091c79f2864
Jack Allen <152200136+allen-j44@users.noreply.github.com>
2024-04-10 15:05:00 +0100

    show all + 1 endpoint

--- BackEnd/app.py
+++ BackEnd/app.py
@@ -18,6 +18,43 @@ CORS(app)
 
 #Call the monog client, assign the Database and Collection
 mongo_client = MongoClient("mongodb://127.0.0.1:27017")
-db = mongo_client["playersDB"]
+db = mongo_client["ProjectDB"]
 collection = db["playersCollection"]
 staff = db.staff
+
+@app.route("/api/v1.0/players", methods=["GET"])
+def show_all_players():
+    page_num, page_size = 1, 5
+    if request.args.get("pn"):
+        page_num = int(request.args.get("pn"))
+    if request.args.get("ps"):
+        page_size = int(request.args.get("ps"))
+    page_start = (page_size * (page_num - 1))
+
+    players = collection.find().skip(page_start).limit(page_size)
+    players_data = []
+
+    for player in players:
+        # Convert ObjectId to string
+        player["_id"] = str(player["_id"])
+        players_data.append(player)
+
+    return make_response(jsonify(players_data), 200)
+
+# Function to retrieve a single player
+@app.route("/api/v1.0/players/<string:id>", methods=["GET"])
+def show_one_player(id):
+    if len(id) != 24 or not all(c in string.hexdigits for c in id):
+        return make_response(jsonify({"error": "Invalid Player ID"}), 404)
+    
+    player = collection.find_one({"_id": ObjectId(id)})
+
+    if player is not None:
+        # Convert ObjectId to string
+        player["_id"] = str(player["_id"])
+        return make_response(jsonify(player), 200)
+    else:
+        return make_response(jsonify({"error": "Player not found"}), 404)
+
+if __name__ == '__main__':
+    app.run(debug=True)
