2d57cfe343b8b0f8d753007d9500b6b0757fe70b
Jack Allen <152200136+allen-j44@users.noreply.github.com>
2024-04-10 15:39:11 +0100

    add + delete endpoints added

--- BackEnd/app.py
+++ BackEnd/app.py
@@ -55,6 +55,57 @@ def show_one_player(id):
         return make_response(jsonify(player), 200)
     else:
         return make_response(jsonify({"error": "Player not found"}), 404)
+    
+# Function to add a new player
+@app.route("/api/v1.0/players", methods=["POST"])
+def add_new_player():
+    # Check if all required fields are present in the request
+    required_fields = ["Team", "Name", "Age", "POS", "App", "Goal Involvements", "Clean Sheets", "Yellows", "Reds", "Mins"]
+    if all(field in request.json for field in required_fields):
+        new_player = {
+            "Team": request.json["Team"],
+            "Name": request.json["Name"],
+            "Age": int(request.json["Age"]),
+            "POS": request.json["POS"],
+            "App": int(request.json["App"]),
+            "Goal Involvements": int(request.json["Goal Involvements"]),
+            "Clean Sheets": int(request.json["Clean Sheets"]),
+            "Yellows": int(request.json["Yellows"]),
+            "Reds": int(request.json["Reds"]),
+            "Mins": int(request.json["Mins"]),
+            # Include other fields as needed
+        }
+
+        # Insert the new player into the collection
+        new_player_id = collection.insert_one(new_player)
+
+        # Fetch the newly inserted player from the database
+        inserted_player = collection.find_one({"_id": new_player_id.inserted_id})
+
+        # Convert ObjectId to string
+        inserted_player["_id"] = str(inserted_player["_id"])
+
+        # Return response with the newly added player
+        return make_response(jsonify(inserted_player), 201)
+    else:
+        return make_response(jsonify({"error": "Missing or invalid form data"}), 400)
+
+@app.route("/api/v1.0/players/<string:id>", methods=["DELETE"])
+def delete_player(id):
+    if len(id) != 24 or not all(c in '0123456789abcdefABCDEF' for c in id):
+        return make_response(jsonify({"error" : f"Invalid Player ID: {id}"}), 400)
+    
+    result = collection.delete_one({"_id" : ObjectId(id)})
+
+    if result.deleted_count == 1:
+        return make_response(jsonify({"message" : f"Player with ID {id} has been deleted"}), 204)
+    else:
+        return make_response(jsonify({"error" : f"Player with ID {id} was not found"}), 404)
+
+    
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
